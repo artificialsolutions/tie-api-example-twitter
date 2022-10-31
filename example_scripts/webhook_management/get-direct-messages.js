665b5c27b37873fe5da61a4af5f14dc76c60e290
JP <juan.gonzalez@artificial-solutions.com>
2022-10-31 15:46:47 +0100

    added direct message

---
+++ example_scripts/webhook_management/get-direct-messages.js
@@ -0,0 +1,16 @@
+const request = require('request-promise')
+const auth = require('../../helpers/auth.js')
+const args = require('../args.js')
+
+
+// request options we will get the direct messages first
+var request_options = {
+  url: 'https://api.twitter.com/1.1/direct_messages/events/list.json',
+  oauth: auth.twitter_oauth
+}
+
+
+// GET request to retreive webhook config
+request.get(request_options, function (error, response, body) {
+  console.log(body)
+})
